efccdb54164393bd6bcfaa9b550a1c1d5f939ca0
itielrivas <31870680+itielrivas@users.noreply.github.com>
2020-06-25 22:01:51 -0500

    Version 1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { RegisterComponent } from './home/register/register.component';
+import { LoginComponent } from './home/login/login.component';
+import { HomeComponent } from './home/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './home/contact/contact.component';
+import { FaqComponent } from './home/faq/faq.component';
+import { AboutComponent } from './home/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { FaqComponent } from './home/faq/faq.component';
+import { AboutComponent } from './home/about/about.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    FaqComponent,
+    AboutComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
